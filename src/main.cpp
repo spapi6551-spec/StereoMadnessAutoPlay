c2ffa5023ad17b79e1512c1f911e621fcdc437a9
spapi6551-spec <spapi6551@gmail.com>
2026-08-14 03:32:23 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/PlayerObject.hpp>
 
 using namespace geode::prelude;
 
+// Noclip: evita las colisiones principales
 class $modify(NoclipPlayerObject, PlayerObject) {
 
     bool collidedWithObject(float dt, GameObject* object) {
@@ -49,6 +51,49 @@ class $modify(NoclipPlayerObject, PlayerObject) {
     void collidedWithSlope(
         float dt,
         GameObject* object,
-        bool 
+        bool skipPre
+    ) {
+        if (Mod::get()->getSettingValue<bool>("enabled"))
+            return;
+
+        PlayerObject::collidedWithSlope(
+            dt,
+            object,
+            skipPre
+        );
+    }
+
+    void collidedWithSlopeInternal(
+        float dt,
+        GameObject* object,
+        bool forced
+    ) {
+        if (Mod::get()->getSettingValue<bool>("enabled"))
+            return;
+
+        PlayerObject::collidedWithSlopeInternal(
+            dt,
+            object,
+            forced
+        );
+    }
+};
+
+// Noclip: evita que el jugador reciba daño
+class $modify(NoclipPlayLayer, PlayLayer) {
+
+    void update(float dt) {
+        PlayLayer::update(dt);
+
+        if (!Mod::get()->getSettingValue<bool>("enabled"))
+            return;
+
+        if (m_player1)
+            m_player1->m_ignoreDamage = true;
+
+        if (m_player2)
+            m_player2->m_ignoreDamage = true;
+    }
+};
 
 
