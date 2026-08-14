b3175689dd86658907b06a50c61bf1a4231aa60d
spapi6551-spec <spapi6551@gmail.com>
2026-08-14 02:46:02 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,64 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/PlayerObject.hpp>
 
 using namespace geode::prelude;
 
-struct InputEvent {
-    float time;
-    bool pressed;
-};
-
-// Eventos de prueba.
-// Después podemos poner aquí los eventos reales de Stereo Madness.
-static constexpr InputEvent EVENTS[] = {
-    {1.00f, true},
-    {1.08f, false},
-
-    {2.10f, true},
-    {2.18f, false},
-
-    {3.25f, true},
-    {3.33f, false},
-
-    {4.45f, true},
-    {4.53f, false},
-
-    {5.70f, true},
-    {5.78f, false},
-
-    {6.95f, true},
-    {7.03f, false},
-
-    {8.20f, true},
-    {8.28f, false},
-
-    {9.50f, true},
-    {9.58f, false},
-
-    {10.80f, true},
-    {10.88f, false},
-
-    {12.10f, true},
-    {12.18f, false}
-};
-
-static float g_autoTime = 0.0f;
-static std::size_t g_nextEvent = 0;
-static bool g_autoPlay = true;
-
-class $modify(StereoMadnessAutoPlay, PlayLayer) {
-
-    bool init(
-        GJGameLevel* level,
-        bool useReplay,
-        bool dontCreateObjects
-    ) {
-        if (!PlayLayer::init(level, useReplay, dontCreateObjects))
+// Noclip para PlayerObject.
+// Cuando el toggle está ON, ignoramos las colisiones.
+class $modify(NoclipPlayerObject, PlayerObject) {
+    bool collidedWithObject(float dt, GameObject* object) {
+        if (Mod::get()->getSettingValue<bool>("noclip")) {
             return false;
-
-        g_autoTime = 0.0f;
-        g_nextEvent = 0;
-        g_autoPlay = true;
-
-        log::info("StereoMadnessAutoPlay loaded!");
-
-        return true;
-    }
-
-    void postUpdate(float dt) {
-        PlayLayer::postUpdate(dt);
-
-        if (!g_autoPlay)
-            return;
-
-        g_autoTime += dt;
-
-        constexpr std::size_t eventCount =
-            sizeof(EVENTS) / sizeof(EVENTS[0]);
-
-        while (
-            g_nextEvent < eventCount &&
-            g_autoTime >= EVENTS[g_nextEvent].time
-        ) {
-            auto const& event = EVENTS[g_nextEvent];
-
-            if (m_player1) {
-                if (event.pressed) {
-                    m_player1->pushButton(PlayerButton::Jump);
-                }
-                else {
-                    m_player1->releaseButton(PlayerButton::Jump);
-                }
-            }
-
-            ++g_nextEvent;
         }
+
+        return PlayerObject::collidedWithObject(dt, object);
     }
 
-    void resetLevel() {
-        PlayLayer::resetLevel();
+    bool collidedWithObject(
+        float dt,
+        GameObject* object,
+        cocos2d::CCRect rect,
+        bool skipCheck
+    ) {
+        if (Mod::get()->getSettingValue<bool>("noclip")) {
+            return false;
+        }
 
-        g_autoTime = 0.0f;
-        g_nextEvent = 0;
-        g_autoPlay = true;
+        return PlayerObject::collidedWithObject(
+            dt,
+            object,
+            rect,
+            skipCheck
+        );
+    }
+
+    bool collidedWithObjectInternal(
+        float dt,
+        GameObject* object,
+        cocos2d::CCRect rect,
+        bool skipCheck
+    ) {
+        if (Mod::get()->getSettingValue<bool>("noclip")) {
+            return false;
+        }
+
+        return PlayerObject::collidedWithObjectInternal(
+            dt,
+            object,
+            rect,
+            skipCheck
+        );
+    }
+};
+
+// Evita que el jugador muera por daño cuando Noclip está ON.
+class $modify(NoclipPlayLayer, PlayLayer) {
+    void playerTookDamage(PlayerObject* player) {
+        if (Mod::get()->getSettingValue<bool>("noclip")) {
+            return;
+        }
+
+        PlayLayer::playerTookDamage(player);
     }
 };
