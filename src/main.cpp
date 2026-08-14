f94291219d9330eeaabf104b2949d7f405727c77
spapi6551-spec <spapi6551@gmail.com>
2026-08-14 03:18:48 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,21 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/PlayerObject.hpp>
 
 using namespace geode::prelude;
 
-// Noclip para PlayerObject.
-// Cuando el toggle está ON, ignoramos las colisiones.
-class $modify(NoclipPlayerObject, PlayerObject) {
-    bool collidedWithObject(float dt, GameObject* object) {
-        if (Mod::get()->getSettingValue<bool>("noclip")) {
-            return false;
-        }
-
-        return PlayerObject::collidedWithObject(dt, object);
-    }
-
-    bool collidedWithObject(
-        float dt,
-        GameObject* object,
-        cocos2d::CCRect rect,
-        bool skipCheck
-    ) {
-        if (Mod::get()->getSettingValue<bool>("noclip")) {
-            return false;
-        }
-
-        return PlayerObject::collidedWithObject(
-            dt,
-            object,
-            rect,
-            skipCheck
-        );
-    }
-
-    bool collidedWithObjectInternal(
-        float dt,
-        GameObject* object,
-        cocos2d::CCRect rect,
-        bool skipCheck
-    ) {
-        if (Mod::get()->getSettingValue<bool>("noclip")) {
-            return false;
-        }
-
-        return PlayerObject::collidedWithObjectInternal(
-            dt,
-            object,
-            rect,
-            skipCheck
-        );
-    }
-};
-
-// Evita que el jugador muera por daño cuando Noclip está ON.
 class $modify(NoclipPlayLayer, PlayLayer) {
-    void playerTookDamage(PlayerObject* player) {
-        if (Mod::get()->getSettingValue<bool>("noclip")) {
-            return;
-        }
+    void update(float dt) {
+        PlayLayer::update(dt);
 
-        PlayLayer::playerTookDamage(player);
+        bool noclip = Mod::get()->getSettingValue<bool>("enabled");
+
+        if (!noclip)
+            return;
+
+        if (m_player1)
+            m_player1->m_ignoreDamage = true;
+
+        if (m_player2)
+            m_player2->m_ignoreDamage = true;
     }
 };
