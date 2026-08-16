3fbd8456def373046fb0a744f5e40dfd1a3e76c2
spapi6551-spec <spapi6551@gmail.com>
2026-08-16 02:01:58 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,24 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
 
 using namespace geode::prelude;
 
-// Noclip: evita las colisiones principales
-class $modify(NoclipPlayerObject, PlayerObject) {
-
-    bool collidedWithObject(float dt, GameObject* object) {
-        if (Mod::get()->getSettingValue<bool>("enabled"))
-            return false;
-
-        return PlayerObject::collidedWithObject(dt, object);
-    }
-
-    bool collidedWithObject(
+class $modify(NoclipGameLayer, GJBaseGameLayer) {
+    int checkCollisions(
+        PlayerObject* player,
         float dt,
-        GameObject* object,
-        cocos2d::CCRect rect,
-        bool skipCheck
+        bool ignoreDamage
     ) {
-        if (Mod::get()->getSettingValue<bool>("enabled"))
-            return false;
+        if (Mod::get()->getSettingValue<bool>("enabled")) {
+            // Ignorar el procesamiento de daño/colisión.
+            return 0;
+        }
 
-        return PlayerObject::collidedWithObject(
+        return GJBaseGameLayer::checkCollisions(
+            player,
             dt,
-            object,
-            rect,
-            skipCheck
+            ignoreDamage
         );
-    }
-
-    bool collidedWithObjectInternal(
-        float dt,
-        GameObject* object,
-        cocos2d::CCRect rect,
-        bool skipCheck
-    ) {
-        if (Mod::get()->getSettingValue<bool>("enabled"))
-            return false;
-
-        return PlayerObject::collidedWithObjectInternal(
-            dt,
-            object,
-            rect,
-            skipCheck
-        );
-    }
-
-    void collidedWithSlope(
-        float dt,
-        GameObject* object,
-        bool skipPre
-    ) {
-        if (Mod::get()->getSettingValue<bool>("enabled"))
-            return;
-
-        PlayerObject::collidedWithSlope(
-            dt,
-            object,
-            skipPre
-        );
-    }
-
-    void collidedWithSlopeInternal(
-        float dt,
-        GameObject* object,
-        bool forced
-    ) {
-        if (Mod::get()->getSettingValue<bool>("enabled"))
-            return;
-
-        PlayerObject::collidedWithSlopeInternal(
-            dt,
-            object,
-            forced
-        );
-    }
-};
-
-// Noclip: evita que el jugador reciba daño
-class $modify(NoclipPlayLayer, PlayLayer) {
-
-    void update(float dt) {
-        PlayLayer::update(dt);
-
-        if (!Mod::get()->getSettingValue<bool>("enabled"))
-            return;
-
-        if (m_player1)
-            m_player1->m_ignoreDamage = true;
-
-        if (m_player2)
-            m_player2->m_ignoreDamage = true;
     }
 };
 
